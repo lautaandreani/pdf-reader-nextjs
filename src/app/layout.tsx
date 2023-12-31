f1592afc80d4f1defb92fc79e25e48fd7d8f8712
lautaroandreani <lautaroandreani2016@gmail.com>
2023-12-31 17:41:14 -0300

    feat: first commmit pdf reader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { NavBar } from '@/components/navbar'
+import { Providers } from '@/components/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,14 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className='light'>
-      <body
-        className={cn(
-          'min-h-screen font-sans antialiased grainy',
-          inter.className
-        )}
-      >
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}
+        >
+          <NavBar />
+          {children}
+        </body>
+      </Providers>
     </html>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,55 @@
+'use client'
+import Link from 'next/link'
+import { MaxWidthWrapper } from './max-width-wrapper'
+import { buttonVariants } from './ui/button'
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs'
+import { ArrowRight } from 'lucide-react'
+
 export function NavBar() {
   return (
-    <nav>
-      <header>
-        <h1>Quill nav</h1>
-      </header>
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+      <MaxWidthWrapper>
+        <header className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link href='/' className='flex z-40 font-semibold'>
+            <span>quill.</span>
+          </Link>
+
+          {/* Mobile nav */}
+
+          <ul className='hidden items-center space-x-4 sm:flex'>
+            <li>
+              <Link
+                href='/pricing'
+                className={buttonVariants({
+                  size: 'sm',
+                  variant: 'ghost',
+                })}
+              >
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <LoginLink
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Sign In
+              </LoginLink>
+            </li>
+            <li>
+              <RegisterLink
+                className={buttonVariants({
+                  size: 'sm',
+                })}
+              >
+                Get Started <ArrowRight className='ml-1.5  size-5' />
+              </RegisterLink>
+            </li>
+          </ul>
+        </header>
+      </MaxWidthWrapper>
     </nav>
   )
 }
